44f3f7e77593a8081b8c90c3947984037fcd1ba5
crist <crist@MSI>
2024-01-08 17:56:52 +0100

    último commit en master

--- PruebagGit/src/main/java/principal/Principal.java
+++ PruebagGit/src/main/java/principal/Principal.java
@@ -11,6 +11,8 @@ public class Principal {
 		
 		System.out.println("Version GitHub");
 		System.out.println("Version Editada por un compañero el domingo");
+		System.out.println("Sigue trabajando en la rama master");
+		
 	}
 
 }
