e55974cca86e320f5e48bd4abcc9f536a17be78c
CrisAvila08 <151665004+CrisAvila08@users.noreply.github.com>
2024-01-08 12:04:20 +0100

    Update Domingo2.java

--- PruebagGit/src/main/java/modelo/MiClase.java
+++ PruebagGit/src/main/java/modelo/MiClase.java
@@ -7,6 +7,8 @@ public class MiClase  implements Miinterfaz{
 	@Override
 	public int metododevuelventero() {
 		// TODO Auto-generated method stub
+		// No me dió tiempo de implementar este método...
+	
 		return 0;
 	}
 
